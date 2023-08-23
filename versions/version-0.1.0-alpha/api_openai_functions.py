bb49a383f1e4c35a86d1cc4c88042faa1f4565b6
J S <99825380+jnshah014@users.noreply.github.com>
2023-08-23 14:20:31 +0100

    Create api_openai_functions.py

---
+++ versions/version-0.1.0-alpha/api_openai_functions.py
@@ -0,0 +1,27 @@
+import openai
+
+openai.api_key = open("api_openai_key", "r").read()
+MODEL_ENGINE = "text-davinci-003"
+TEMPERATURE = 0.7
+
+def txt_generate(user_input):
+    completion = openai.Completion.create(
+        engine=MODEL_ENGINE,
+        prompt=user_input,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=TEMPERATURE,
+    )
+    response = completion.choices[0].text
+    return response
+
+def img_generate(user_input):
+    print("[GAME] Loading image...")
+    response = openai.Image.create(
+        prompt=user_input,
+        n=1,
+        size="1024x1024"
+    )
+    image_url = response['data'][0]['url']
+    print("[IMAGE URL] " + image_url)
